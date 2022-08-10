19518bd5a1ae82a53c84063e3d400769de854ab0
Nougmanov Boulat <nugmanov.bn@phystech.edu>
2022-08-10 22:43:52 +0300

    re_along_curve теперь можно делать по аналитической и численной функции

--- steepest_descent/re_along_curve.py
+++ steepest_descent/re_along_curve.py
@@ -1,10 +1,15 @@
 import numpy as np
 import numpy.typing as npt
 import sympy as sp
+from typing import Callable
 
 
-def re_along_curve(z: sp.Symbol,
-                   analytic_func: sp.Expr,
-                   points: npt.NDArray[tuple[float, float]]) -> npt.NDArray[float]:
+def analytic_re_along_curve(z: sp.Symbol,
+                            analytic_func: sp.Expr,
+                            points: npt.NDArray[tuple[float, float]]) -> npt.NDArray[float]:
     num_func = sp.lambdify(z, analytic_func)
+    return numeric_re_along_curve(num_func, points)
+
+
+def numeric_re_along_curve(num_func: Callable, points: npt.NDArray[tuple[float, float]]) -> npt.NDArray[float]:
     return np.array(tuple(map(lambda point: np.real(num_func(point[0] + 1j * point[1])), points)))
