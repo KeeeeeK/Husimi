6857ffa575a0ccd9c68f49b4e725e9dea6ee3d40
Nougmanov Boulat <nugmanov.bn@phystech.edu>
2022-08-14 20:07:53 +0300

    Добавил функцию для поиска значений в седловых точках

--- steepest_descent/F_research.py
+++ steepest_descent/F_research.py
@@ -5,7 +5,7 @@ import numpy.typing as npt
 from typing import Callable
 
 from constant_phase_curve import constant_phase_curve
-
+from re_along_curve import numeric_re_along_curve
 
 def F(alpha_gamma: complex) -> Callable[[complex], complex]:
     return lambda z: -1j * z ** 2 + alpha_gamma * np.exp(2 * 1j * z)
@@ -24,3 +24,12 @@ def F_const_phase_curve(alpha_gamma: complex, k: int, steps_params=(0.1, 100, 10
     analytic_func = -1j * z ** 2 + alpha_gamma * sp.exp(2 * 1j * z)
     x_k, y_k = F_decent_point(alpha_gamma, k)
     return constant_phase_curve(z, analytic_func, (x_k, y_k), steps_params=steps_params)
+
+def F_values_in_saddle_points(n_dots: int, alpha_gamma: complex) -> npt.NDArray[float]:
+    """
+    :param n_dots: число седловых точек
+    :param alpha_gamma: alpha*gamma
+    :return: Значения в F в седловых точках под номерами (0,-1, -2,..., -n_dots+1)
+    """
+    points = np.array([F_decent_point(alpha_gamma, -k) for k in range(n_dots)])
+    return numeric_re_along_curve(F(alpha_gamma), points)
