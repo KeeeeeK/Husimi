d3c3020650131233e9456fceba4532aaae65c37a
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2022-10-22 01:48:35 +0300

    Заготовка для нахождения областей главных ветвей

---
+++ branch_summation/where_main_branch.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy as sc
+
+
+def _next_point_and_direction():
+    ...
+
+
+def _step_algorithm():
+    ...
+
+
+def _solve_curve(f, step):
+    ...
+
+
+def _main_branch_curves(k: int, epsilon: float):
+    ...
+
+
+def plot_main_branches_by_z():
+    ...
+
+
+if __name__ == '__main__':
+    ...
