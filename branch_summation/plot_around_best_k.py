91ac8a73487c911d02082fa38bf78841e8f4f314
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2022-10-28 03:07:33 +0300

    Теперь можно посмотреть за множеством нулей наглядно!

--- branch_summation/plot_around_best_k.py
+++ branch_summation/plot_around_best_k.py
@@ -8,8 +8,8 @@ def z_k_approx(Z, k):
     L2 = np.log(L1)
     return 1j * (L1 - L2 + L2 / L1 +
                  L2 * (-2 + L2) / 2 / L1 ** 2 + 0)
-                 # L2 * (6 - 9 * L2 + 2 * L2 ** 2) / 6/ L1 ** 3 +
-                 # L2 * (-12 + 36*L2 -22*L2**2 + 3*L2**3) / 12 / L1**4)
+    # L2 * (6 - 9 * L2 + 2 * L2 ** 2) / 6/ L1 ** 3 +
+    # L2 * (-12 + 36*L2 -22*L2**2 + 3*L2**3) / 12 / L1**4)
 
 
 def z_k_exact(Z, k):
@@ -20,19 +20,22 @@ def f_in_z_k(Z, k, z_k_func=z_k_approx):
     z_k = z_k_func(Z, k)
     return z_k ** 2 / (2 * 1j) + z_k
 
+
 def k_optimal(R, Gamma_sign):
-    return -int(Gamma_sign*np.round(R / (2*np.pi)))
+    return -int(Gamma_sign * np.round(R / (2 * np.pi)))
+
 
 def F_plot(R, Gamma):
     k = k_optimal(R, np.sign(Gamma))
-    x = np.linspace(-np.pi,  np.pi, 100)
+    x = np.linspace(-np.pi, np.pi, 100)
     Z = R * np.exp(1j * x)
     y_approx = np.exp((np.real(f_in_z_k(Z, k, z_k_func=z_k_approx)) + R * np.sign(k)) / (2 * Gamma))
     y_exact = np.exp((np.real(f_in_z_k(Z, k, z_k_func=z_k_exact)) + R * np.sign(k)) / (2 * Gamma))
     # / np.abs(np.sqrt(1+W_approx(Z, k)))
-    plt.plot(x, y_exact, label = 'exact')
+    plt.plot(x, y_exact, label='exact')
     plt.plot(x, y_approx, label='approx')
     plt.legend()
+
 
 F_plot(100, 10 ** -2)
 plt.show()
--- branch_summation/where_main_branch.py
+++ branch_summation/where_main_branch.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sc
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 
 def _next_point_and_direction():
@@ -21,6 +23,15 @@ def _main_branch_curves(k: int, epsilon: float):
 def plot_main_branches_by_z():
     ...
 
+def plot_sign_f(f, x_step_params, y_step_params):
+    X, Y = np.mgrid[x_step_params[0]:x_step_params[1]:complex(0, x_step_params[2]),
+                    y_step_params[0]:y_step_params[1]:complex(0, y_step_params[2])]
+    Z = np.sign(f(X, Y))
+    fig, ax = plt.subplots(1, 1)
+    pcm = ax.pcolor(X, Y, Z,
+                    cmap='inferno', shading='nearest')
+
 
 if __name__ == '__main__':
-    ...
+    plot_sign_f(lambda x, y: x**2 + y**2 - 1, (-2, 2, 100), (-3, 3, 200))
+    plt.show()
