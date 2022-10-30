b631f5c9dd6e9b8ddc4ecdf9d54b52fecb1b5e39
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2022-10-30 04:59:20 +0300

    Теперь есть графики k_best(Z)

--- branch_summation/where_main_branch.py
+++ branch_summation/where_main_branch.py
@@ -29,14 +29,20 @@ def _best_k(Z, k_sign):
     k_max = k_arr[np.argmax(np.real(f_in_z_k * (-k_sign)))]
     return k_max
 
-def plot_best_k(x_step_params, y_step_params):
+def plot_best_k(x_step_params, y_step_params, k_sign):
     # print(tuple(it.product(np.arange(x_step_params[2]), np.arange(y_step_params[2]))))
     X, Y = np.mgrid[x_step_params[0]:x_step_params[1]:complex(0, x_step_params[2]),
            y_step_params[0]:y_step_params[1]:complex(0, y_step_params[2])]
+    K = np.zeros((x_step_params[2], y_step_params[2]))
+    for i in range(x_step_params[2]):
+        for j in range(y_step_params[2]):
+            # следующая версия правильна, но если комплексно сопрячь, то результат не зависит от k_sign
+            # K[i, j] = _best_k(X[i, j] + 1j * Y[i, j], k_sign)
+            K[i, j] = _best_k(X[i, j] + 1j*Y[i, j]*k_sign, k_sign)
     # Z = [i+k for i, k in it.product(np.arange(x_step_params[2]), np.arange(y_step_params[2]))]
-    fig, ax = plt.subplots(1, 1)
-    ax.pcolor(X, Y, Z,
-              cmap='inferno', shading='nearest')
+    ax = plt.gca()
+    ax.pcolor(X, Y, K,
+              cmap='inferno', shading='nearest', alpha=0.5)
 
 
 def plot_sign_f(f, x_step_params, y_step_params):
@@ -49,7 +55,6 @@ def plot_sign_f(f, x_step_params, y_step_params):
 
 
 if __name__ == '__main__':
-    # plot_sign_f(lambda x, y: x**2 + y**2 - 1, (-2, 2, 100), (-3, 3, 200))
-    print(_best_k(10, 1))
-    # plot_best_k((-2, 2, 10), (-3, 3, 20))
-    # plt.show()
+    plot_best_k((-10, 10, 200), (-10, 10, 200), 1)
+    plot_best_k((-10, 10, 200), (-10, 10, 200), -1)
+    plt.show()
