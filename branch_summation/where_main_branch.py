089747096c7b6916b81e39dc5de1802c53860ebf
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2022-11-06 02:11:53 +0300

    Теперь есть точный график границ областей оптимальности

--- branch_summation/where_main_branch.py
+++ branch_summation/where_main_branch.py
@@ -8,13 +8,19 @@ def _step_algorithm(f, r_min, r_max, freq):
     phi_arr = np.linspace(-np.pi, np.pi, freq)
     r_opt = np.array(tuple(sc.optimize.root_scalar(
         lambda r: f(r*np.exp(1j*phi)), x0=r_min, x1=r_max).root for phi in phi_arr))
-    return r_opt
+    return r_opt, phi_arr
 
 
-def _main_branch_curves(k: int, epsilon: float):
-    ...
 
-
+def plot_borders(k, freq):
+    r_approx = 2 * np.pi * np.abs(k) + np.pi / 2
+    # теперь надо найти для каждого фи такую эр, чтоб была граница между той областью, где брать оптимально k и k+1.
+    # для этого сначала напишем re_f_in_z_k, чтоб по Z и k определять значение f
+    def re_f_in_z_k(Z, k):
+        z_k = 1j*sc.special.lambertw(Z, k=k)
+        return np.real(z_k ** 2 / 2j + z_k)
+    r_arr, phi_arr = _step_algorithm(lambda z: re_f_in_z_k(z, k) - re_f_in_z_k(z, k+1), r_approx, r_approx+2, freq)
+    plot_polar(r_arr, phi_arr)
 
 def _best_k_slow(Z, k_sign):
     k_mean_abs, k_range = np.int_(np.abs(Z)/(2*np.pi)), 5
@@ -48,9 +54,9 @@ def plot_sign_f(f, x_step_params, y_step_params):
     ax.pcolor(X, Y, Z,
                     cmap='inferno', shading='nearest')
 
-def plot_polar(r_arr, phi_arr):
+def plot_polar(r_arr, phi_arr, c=None):
     # plt.polar - отстой
-    plt.plot(r_arr*np.cos(phi_arr), r_arr*np.sin(phi_arr))
+    plt.plot(r_arr*np.cos(phi_arr), r_arr*np.sin(phi_arr), c=c)
 
 
 def plot_where_biggest_f(r_max, k_sign):
@@ -58,13 +64,23 @@ def plot_where_biggest_f(r_max, k_sign):
     phi_arr = k_sign * (r_arr + np.pi /2)
     plot_polar(r_arr, phi_arr)
 
+
+def plot_asymptotic_borders(k, freq):
+    phi_arr = np.linspace(-np.pi, np.pi, freq)
+    r_arr = 2*np.pi*np.abs(k) + phi_arr * np.sign(k) + np.pi/2
+    plot_polar(r_arr, phi_arr, c='red')
+
 if __name__ == '__main__':
     # print(_step_algorithm(lambda z: np.abs(z)-1.2, 1, 2, 10))
 
-    Z_range, freq  = 100, 400
-    plot_best_k((-Z_range, Z_range, freq), (-Z_range, Z_range, freq), -1)
-    plot_where_biggest_f(Z_range, -1)
+    Z_range, freq  = 10, 400
+    plot_best_k((-Z_range, Z_range, freq), (-Z_range, Z_range, freq), 1)
+    # plot_where_biggest_f(Z_range, -1)
+    # for k_abs in range(int(Z_range / 2/np.pi)):
+    #     plot_borders(k_abs + 0.0000001, 100)
+    # _main_branch_curves(1)
     # plot_best_k((-30, 1, freq), (-0.01, 0.01, 20), -1)
+    plot_borders(0, 100)
     plt.show()
 
     # phi = np.linspace(-np.pi, np.pi)
