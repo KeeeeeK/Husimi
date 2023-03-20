4758d0f5b460b23abde6802fb96711d6bb5999ce
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2023-03-21 01:43:39 +0300

    Показал, что формула "в тупую" работает только при alpha_abs<1

--- branch_summation/wigner_calculus.py
+++ branch_summation/wigner_calculus.py
@@ -1,13 +1,17 @@
 import numpy as np
 import numba as nb
-
+import matplotlib.pyplot as plt
 from numerical_summation.nb_sum_fn import _sum_fn as Fn
 
 
 # это алгоритм для F normalized. Она должна быть vectorized.
 # Fn = lambda r, phi, gamma: sum_fn(r, phi, gamma, 3)
+def Fn_zero_gamma(r, phi, gamma, n_sigma):
+    return np.exp(r*np.exp(1j*phi) - r)
 
 def np_wigner(alpha_abs, alpha_arg, beta_abs, beta_arg, Gamma, n_sigma):
+    print(alpha_abs)
+    Fn = Fn_zero_gamma
     type_ = np.double
     m_min = max((np.int_(3), np.int_(np.round(alpha_abs ** 2 - n_sigma * alpha_abs))))
     m_max = np.int_(np.round(alpha_abs ** 2 + n_sigma * alpha_abs))
@@ -18,20 +22,19 @@ def np_wigner(alpha_abs, alpha_arg, beta_abs, beta_arg, Gamma, n_sigma):
     minus_one_powers = 1 - 2 * np.mod(m_arr, 2)
     coefficients_to_exp = -half_log_2pi - 1 / 2 * log_m_arr - m_arr * (
                 log_m_arr - 1 - 2 * np.log(alpha_abs)) - 1 / 12 / m_arr
+
     Fn_sq_arr = np.abs(
         Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg + (2 * m_arr - 2) * Gamma, Gamma, n_sigma)) ** 2
-
-    Fn_sq_arr = type_(Fn_sq_arr)
+    # print(np.exp(coefficients_to_exp - m_arr * 2 * np.log(alpha_abs)) ** -1) # с хорошей точностью это действительно факториалы
+    # Fn_sq_arr = type_(Fn_sq_arr)
 
     partial_sum = 0
     for m_shifted in range(m_max - m_min):
         partial_sum += minus_one_powers[m_shifted] * np.exp(coefficients_to_exp[m_shifted]) * Fn_sq_arr[m_shifted]
     additional_sum = np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg - 2 * Gamma, Gamma, n_sigma)) ** 2 + \
-                     (-alpha_abs ** 2) * np.abs(
-        Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg, Gamma, n_sigma)) ** 2 + \
-                     alpha_abs ** 4 / 2 * np.abs(
-        Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg + 2 * Gamma, Gamma, n_sigma)) ** 2
-    return 2 / np.pi * np.exp(-2 * beta_abs ** 2 - alpha_abs ** 2 + 4 * alpha_abs * beta_abs) * \
+     (-alpha_abs ** 2) * np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg, Gamma, n_sigma)) ** 2 + \
+     alpha_abs ** 4 / 2 * np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg + 2 * Gamma, Gamma, n_sigma)) ** 2
+    return 2 / np.pi * np.exp(alpha_abs ** 2 - 2 * (alpha_abs - beta_abs)**2) * \
         (additional_sum + partial_sum)
 
 
@@ -54,15 +57,19 @@ def nb_wigner(alpha_abs, alpha_arg, beta_abs, beta_arg, Gamma, n_sigma):
     additional_sum = np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg - 2 * Gamma, Gamma, n_sigma)) ** 2 + \
      (-alpha_abs ** 2) * np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg, Gamma, n_sigma)) ** 2 + \
      alpha_abs ** 4 / 2 * np.abs(Fn(2 * alpha_abs * beta_abs, alpha_arg - beta_arg + 2 * Gamma, Gamma, n_sigma)) ** 2
-    return 2 / np.pi * np.exp(-2 * beta_abs ** 2 - alpha_abs ** 2 + 4 * alpha_abs * beta_abs) *\
+    return 2 / np.pi * np.exp(alpha_abs ** 2 - 2* (alpha_abs - beta_abs)**2) *\
         (additional_sum + partial_sum)
 
 
 if __name__ == '__main__':
     # print(nb_wigner(10, 1, np.array([10, 2]), np.array([1, 2]), 0, 100))
+    alpha_abs = 0.345
+    print(np_wigner(alpha_abs, 1, alpha_abs, 1, 0, 10) * np.pi/2)
 
-    print(np_wigner(5, 1, 1, 1, 0, 10))
-    # print(nb_weak_wigner(5, 1, 1, 1, 0, 10))
-    # print(nb_wigner(5, 1, 1, 1, 0, 10))
+    x = np.linspace(0.1, 1, 100)
+    y = [np_wigner(xi, 1, xi, 1, 0, 10) * np.pi/2 - 1 for xi in x]
+    plt.plot(x, y)
+    plt.axhline(0, 0, np.max(x))
+    plt.show()
 
     # print(np.log(np.abs(nb_wigner(10, 1, 2, 2.4, 0.01, 8))))
--- branch_summation/wigner_plot.py
+++ branch_summation/wigner_plot.py
@@ -6,8 +6,10 @@ def test_plot_wigner(x_step_params, y_step_params, alpha, gamma, n_sigma):
     X, Y = np.mgrid[x_step_params[0]:x_step_params[1]:complex(0, x_step_params[2]),
                     y_step_params[0]:y_step_params[1]:complex(0, y_step_params[2])]
     W = wigner(np.abs(alpha), np.angle(alpha), np.abs(X+1j*Y), np.angle(X+1j*Y), gamma, n_sigma)
+    W0 = 1/np.pi*np.exp(-2*np.abs(alpha - (X+1j*Y))**2)
     ax = plt.figure().add_subplot(111, projection='3d')
     ax.plot_surface(X, Y, W, cmap='inferno')
+    # ax.plot_surface(Y, X, W0, cmap='inferno')
 
 
 def n_sigma_dependence():
@@ -17,9 +19,9 @@ def n_sigma_dependence():
 
 
 if __name__ == '__main__':
-    alpha = 10 ** (2/3)
+    alpha = 2
 
-    xy_range = alpha + np.sqrt(np.abs(alpha))+1
+    xy_range = alpha + np.sqrt(np.abs(alpha))+0.5
     x_range, y_range, freq = xy_range, xy_range, 20
     test_plot_wigner((-x_range, x_range, freq), (-y_range, y_range, freq), alpha, 0, 100)
 
